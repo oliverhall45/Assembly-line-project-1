8b13edd46c6538fa7b49bace438addcaf39dfb66
oliverhall45 <halloli@sheridancollege.ca>
2025-07-13 18:49:03 -0400

    Made the ball bounce from 2 points
    Made the ball bounce from the button to where the bat will hit it.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    public float pointA;
+    public float pointB;
+    public float speed = 0.02f;
+
+    private Vector3 startPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 newPosition = transform.position + Vector3.right * speed;
+
+        transform.position = newPosition;
+
+        bool buttonHit = transform.position.x > pointA;
+
+        if (buttonHit)
+        {
+            speed = speed - 0.04f;
+        }
+
+        bool batHit = transform.position.x < pointB;
+
+        if (batHit)
+        {
+            speed = speed + 0.04f;
+        }
     }
 }
